201767d50337e19d28cbdafd1c7119d2bd940854
AraoSousa <aarao.sousa@estudante.ufjf.br>
2024-01-25 16:44:28 -0300

    events

--- events.js
+++ events.js
@@ -1,16 +1,46 @@
-const  eventEmitter =  require('events');
+const  EventEmitter =  require('events');
+const http = require('http');
 
-const myEmitter =  new eventEmitter();
+class Sales extends EventEmitter     {
+    constructor()   {
+        super();
+    }
+}
+
+const myEmitter =  new Sales();
+
 
 myEmitter.on('newSale',() => {
-console.log('There was a new sale!');
+    console.log('There was a new sale!');
 });
 
 myEmitter.on('newSale',() => {
-console.log('Costumer name: Aarão');
+    console.log('Costumer name: Aarão');
 });
 
 myEmitter.on('newSale', stock => {
     console.log(`There are now ${stock} itens left in stock.`);
-})
+});
 myEmitter.emit('newSale', 9);
+ 
+/////////////////////////////////////////OUVIR DIFERENTER EVENTOS 
+
+const server = http.createServer()
+
+server.on('request', (req, res) => {
+    console.log('request received');
+    console.log(req.url);
+    res.end('request received');
+});
+
+server.on('request', (req, res) => {
+  console.log('Another request😊');
+    
+});
+server.on('close', () => {
+console.log('Server Closed');
+});
+
+server.listen(8000, '127.0.0.1', () =>{
+ console.log("waiting for requests...");
+});
