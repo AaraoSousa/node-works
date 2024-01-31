768003b2429eb0192ff7f3003159e858ce42bac5
AraoSousa <aarao.sousa@estudante.ufjf.br>
2024-01-31 11:13:47 -0300

    modules.exports

--- modules.js
+++ modules.js
@@ -1,1 +1,9 @@
-console.log(arguments);
+// console.log(arguments);
+// console.log(require('module').wrapper);
+
+//modules.exports
+const C = require('./test-module-1');
+const calc1 = new C();
+console.log(calc1.add(2, 4));
+
+// exports
